587c2eb75d3bf95df8c41407fa2b229ccc79b1de
Adam Grady <adam.grady@live.com>
2021-08-25 23:24:58 -0500

    Take Two Stones

---
+++ TakeTwoStones/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TakeTwoStones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            if (n % 2 != 0)
+                Console.WriteLine("Alice");
+            else
+                Console.WriteLine("Bob");
+        }
+    }
+}
