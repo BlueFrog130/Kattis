4897f692f8f120e81e98b7bd37e59984a4637864
Adam Grady <adam.grady@live.com>
2021-08-25 22:21:51 -0500

    Tarifa

---
+++ Tarifa/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tarifa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            int megabytes = x;
+            for (int i = 0; i < n; i++)
+            {
+                int used = int.Parse(Console.ReadLine());
+                megabytes += x - used;
+            }
+            Console.WriteLine(megabytes);
+        }
+    }
+}
