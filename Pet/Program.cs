c8d98f214ebacbec6da0e8843e61a1c04cbe6183
Adam Grady <adam.grady@live.com>
2021-08-25 22:21:22 -0500

    Pet

---
+++ Pet/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Pet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, IEnumerable<int>> contestants = new Dictionary<int, IEnumerable<int>>();
+            int i = 1;
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                contestants.Add(i, line.Split(" ").Select(n => int.Parse(n)));
+                i++;
+            }
+            int winner = contestants.Max(c => c.Value.Sum());
+            int key = contestants.First(c => c.Value.Sum() == winner).Key;
+
+            Console.WriteLine($"{key} {winner}");
+        }
+    }
+}
