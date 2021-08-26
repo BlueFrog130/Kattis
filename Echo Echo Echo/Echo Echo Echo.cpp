b0f7958323eac1d7fbf6af835669360b9958c2d0
Adam Grady <adam.grady@live.com>
2021-08-25 21:25:56 -0500

    Echo Echo Echo

---
+++ Echo Echo Echo/Echo Echo Echo.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string word;
+    cin >> word;
+    for (int i = 0; i < 3; i++)
+    {
+        cout << word << " ";
+    }
+}
