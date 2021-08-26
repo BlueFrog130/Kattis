33b360062385073867a2c7749c3a8b71276dc03a
Adam Grady <adam.grady@live.com>
2021-08-25 22:21:32 -0500

    Solving for Carrots

---
+++ SolvingForCarrots/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SolvingForCarrots
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> input = Console.ReadLine().Split(" ").Select(s => int.Parse(s));
+            int n = input.First();
+            int p = input.Last();
+
+            for (int i = 0; i < n; i++)
+                Console.ReadLine();
+
+            Console.WriteLine(p);
+        }
+    }
+}
