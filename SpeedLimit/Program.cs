2cb6730a9a6bba56b298c86423f78eae2e9fe233
Adam Grady <Adam.Grady@live.com>
2021-08-27 10:46:06 -0500

    Speed Limit

---
+++ SpeedLimit/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SpeedLimit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            if (n == -1)
+                return;
+
+            int miles = 0;
+            int time = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] chunks = Console.ReadLine().Split(" ");
+                int s = int.Parse(chunks[0]);
+                int t = int.Parse(chunks[1]);
+                miles += s * (t - time);
+                time = t;
+            }
+
+            Console.WriteLine($"{miles} miles");
+
+            Main(args);
+        }
+    }
+}
