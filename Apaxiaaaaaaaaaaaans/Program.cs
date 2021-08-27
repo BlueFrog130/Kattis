c02ca5ce6c1b218f0dfaf55abf9ad56adcff26c6
Adam Grady <Adam.Grady@live.com>
2021-08-27 10:46:22 -0500

    Apaxiaaaaaaaaaaaans

---
+++ Apaxiaaaaaaaaaaaans/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace Apaxiaaaaaaaaaaaans
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Console.ReadLine().ToCharArray().Aggregate("", (str, c) => str.Length > 0 && str.Last() == c ? str : str + c));
+        }
+    }
+}
