1d7165da527d7011eb536eb8b8c01894c4441567
Adam Grady <Adam.Grady@live.com>
2021-09-03 14:07:23 -0500

    missing numbers

---
+++ MissingNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MissingNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> nums = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                nums.Add(int.Parse(Console.ReadLine()));
+            }
+
+            IEnumerable<int> expected = Enumerable.Range(1, nums.Max());
+
+            IEnumerable<int> difference = expected.Except(nums);
+
+            if (!difference.Any())
+            {
+                Console.WriteLine("good job");
+                return;
+            }
+
+            foreach (int d in difference)
+            {
+                Console.WriteLine(d);
+            }
+        }
+    }
+}
