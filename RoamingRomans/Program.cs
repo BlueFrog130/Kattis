94f21d8db199d1f2fdc5575a6f90d2fb245e99be
Adam Grady <Adam.Grady@live.com>
2021-09-03 15:06:53 -0500

    codeclub

---
+++ RoamingRomans/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RoamingRomans
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = double.Parse(Console.ReadLine());
+            double ratio = 5280.0 / 4854.0;
+
+            Console.WriteLine(Math.Round(x * 1000 * ratio));
+        }
+    }
+}
---
+++ Vauvau/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Vauvau
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // [A B C D]
+            List<int> abcd = Console.ReadLine().Split(" ").Select(n => int.Parse(n)).ToList();
+
+            Doge d1 = new Doge(abcd[0], abcd[1]);
+            Doge d2 = new Doge(abcd[2], abcd[3]);
+
+            // [P M G]
+            IEnumerable<int> pmg = Console.ReadLine().Split(" ").Select(n => int.Parse(n));
+
+
+
+        }
+
+        class Doge
+        {
+            public int Aggressive { get; }
+            public int Calm { get; }
+
+            public Doge(int a, int c)
+            {
+                Aggressive = a;
+                Calm = c;
+            }
+
+            public bool IsAgro(int time)
+            {
+                int t = 0;
+                bool agro = true;
+                while (true)
+                {
+
+                }
+            }
+        }
+    }
+}
