2f6bf3df7d27745d6729fd4e8f638b11f65ab3b8
Adam Grady <adam.grady@live.com>
2021-09-13 18:24:36 -0500

    stopwatch

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Stopwatch
+{
+    /// <summary>
+    /// https://open.kattis.com/problems/stopwatch
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int t = 0;
+            int last = int.Parse(Console.ReadLine());
+            for (int i = 1; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (i % 2 != 0)
+                    t += num - last;
+
+                last = num;
+            }
+
+            if (n % 2 != 0)
+                Console.WriteLine("still running");
+            else
+                Console.WriteLine(t);
+        }
+    }
+}
