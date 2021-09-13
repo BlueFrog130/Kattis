2968f2fe9f0d21b0e465304568aa26481dfd55fd
Adam Grady <adam.grady@live.com>
2021-09-13 12:14:59 -0500

    attempt at string matching

---
+++ StringMatching/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace StringMatching
+{
+    /// <summary>
+    /// https://open.kattis.com/problems/stringmatching
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern;
+            while (!string.IsNullOrEmpty(pattern = Console.ReadLine()))
+            {
+                string text = Console.ReadLine();
+                int offset = 0;
+                int i;
+                while ((i = GetMatchedIndex(ref text, ref pattern, offset)) != -1)
+                {
+                    Console.Write($"{i} ");
+                    offset = i + 1;
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static int GetMatchedIndex(ref string s, ref string pattern, int from)
+        {
+            string sub = s.Substring(from);
+            int idx = sub.IndexOf(pattern);
+            if (idx == -1)
+                return -1;
+            return idx + from;
+        }
+    }
+}
