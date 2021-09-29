4d64f40cc6c6d8010ccd1cc63bee3ad378598f6c
Adam Grady <adam.grady@live.com>
2021-09-29 13:02:50 -0500

    Starting 2 problems

---
+++ CaloriesFromFat/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CaloriesFromFat
+{
+    class Program
+    {
+        static readonly int MAX_CALORIES = 2000;
+
+        static void Main(string[] args)
+        {
+            string last = null;
+            string line;
+            while ((line = Console.ReadLine()) != last)
+            {
+                string[] split = line.Split(' ');
+                
+            }
+        }
+
+        static int Parse(string[] input)
+        {
+
+        }
+    }
+}
---
+++ MillionaireMadness/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace MillionaireMadness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split(" ");
+            int m = int.Parse(line[0]);
+            int n = int.Parse(line[1]);
+
+            int[,] vault = new int[m,n];
+
+            for (int i = 0; i < m; i++)
+            {
+                int[] nums = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+                for (int j = 0; j < n; j++)
+                    vault[i, j] = nums[j];
+            }
+
+            int[] x = { 0, 0, 1, -1 };
+            int[] y = { 1, -1, 0, 0 };
+        }
+    }
+}
