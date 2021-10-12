6db312ffb25e79c2f070975caafc165eda17df52
Adam Grady <Adam.Grady@live.com>
2021-10-12 18:30:33 -0500

    Code club start

---
+++ EulerianGraphs/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EulerianGraphs
+{
+    /// <summary>
+    /// https://open.kattis.com/problems/eulerian
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> graph = Console.ReadLine().Split(" ").Select(x => int.Parse(x));
+            Node[] nodes = new Node[graph.First()];
+            for (int i = 0; i < nodes.Length; i++)
+            {
+                nodes[i] = new Node();
+            }
+            int edges = graph.Last();
+            for (int i = 0; i < edges; i++)
+            {
+                IEnumerable<int> edge = Console.ReadLine().Split(" ").Select(x => int.Parse(x));
+                nodes[edge.First() - 1].Edges.Add(new Edge(nodes[edge.Last() - 1]));
+            }
+            IEnumerable<bool> t = nodes.Select(n => n.Traversable(edges));
+        }
+    }
+
+    class Node
+    {
+        public List<Edge> Edges { get; }
+        public Node()
+        {
+            Edges = new List<Edge>();
+        }
+
+        public bool Traversable(int totalEdges)
+        {
+            HashSet<Edge> visited = new HashSet<Edge>();
+            Queue<Node> q = new Queue<Node>();
+
+            q.Enqueue(this);
+
+            while (q.Count > 0)
+            {
+                Node u = q.Dequeue();
+                foreach (Edge e in u.Edges)
+                {
+                    if (!visited.Contains(e))
+                    {
+                        visited.Add(e);
+                        q.Enqueue(e.To);
+                    }
+                }
+            }
+
+            return visited.Count == totalEdges;
+        }
+    }
+
+    class Edge
+    {
+        public Node To { get; }
+
+        public Edge(Node node)
+        {
+            To = node;
+        }
+    }
+}
---
+++ Hermits/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hermits
+{
+    /// <summary>
+    /// https://open.kattis.com/problems/hermits
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Street[] streets = Console.ReadLine().Split(" ").Select((x, j) => new Street(j + 1, int.Parse(x))).ToArray();
+            int m = int.Parse(Console.ReadLine());
+            for (int i = 0; i < m; i++)
+            {
+                IEnumerable<int> pair = Console.ReadLine().Split(" ").Select(x => int.Parse(x));
+                streets[pair.First() - 1].Crosses.Add(streets[pair.Last() - 1]);
+                streets[pair.Last() - 1].Crosses.Add(streets[pair.First() - 1]);
+            }
+            Street best = streets.Aggregate((current, next) => current.GetPeople() > next.GetPeople() ? next : current);
+            Console.WriteLine(best.S);
+        }
+
+        class Street
+        {
+            public int S { get; }
+            public int People { get; }
+
+            public List<Street> Crosses { get; }
+
+            public Street(int s, int p)
+            {
+                S = s;
+                People = p;
+                Crosses = new List<Street>();
+            }
+
+            public int GetPeople()
+            {
+                return People + Crosses.Sum(c => c.People);
+            }
+        }
+    }
+}
