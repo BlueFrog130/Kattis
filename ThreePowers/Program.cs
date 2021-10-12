86bac8179ef89394ce4c32c45c1ab1dba1c84fd8
Adam Grady <adam.grady@live.com>
2021-10-12 18:56:55 -0500

    Three Powers

---
+++ ThreePowers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace ThreePowers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            while ((line = Console.ReadLine()) != "0")
+                Solve(ulong.Parse(line) - 1);
+        }
+
+        static void Solve(ulong n)
+        {
+            List<BigInteger> v = new List<BigInteger>();
+            BigInteger c = 1;
+            while (n > 0)
+            {
+                if (n % 2 == 1)
+                    v.Add(c);
+                c *= 3;
+                n /= 2;
+            }
+            string nums = string.Join(", ", v.Select(x => x.ToString()));
+            Console.WriteLine("{ " + nums + " }");
+        }
+    }
+}
