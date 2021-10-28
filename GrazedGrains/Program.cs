5fa36a761cb1b63c21a330c32e5b8b8ccae49f4d
Adam Grady <Adam.Grady@live.com>
2021-10-28 09:45:42 -0500

    code club

---
+++ GrazedGrains/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+
+namespace GrazedGrains
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Circle[] circles = new Circle[n];
+            for (int i = 0; i < n; i++)
+            {
+                int[] line = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+                circles[i] = new Circle(line[0], line[1], line[2]); 
+            }
+            Console.WriteLine(sum);
+        }
+    }
+
+    struct Circle
+    {
+        public Point Center;
+        public int Radius;
+        public Circle(int x, int y, int radius)
+        {
+            Center = new Point(x, y);
+            Radius = radius;
+        }
+    }
+}
---
+++ ThePlank/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ThePlank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] sums = new int[n + 1];
+            sums[0] = 1;
+            for (int i = 1; i <= n; i++)
+                for (int j = 1; j <= 3; j++)
+                    if (i >= j)
+                        sums[i] += sums[i - j];
+            Console.WriteLine(sums[n]);
+        }
+    }
+}
